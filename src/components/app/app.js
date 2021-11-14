b51c15fdb255a95a4386e514504f03328e9a7a73
ksusa <k.ero00@mail.ru>
2021-11-14 18:11:25 +0500

    Странички тестов пока не все

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -4,13 +4,16 @@ import EditPage from '../editPage/editPage';
 import EnterPage from '../enterPage/enterPage';
 import DialogsPage from '../dialogs/dialogsPage';
 import RegistrationPage from '../registrationPage/registrationPage';
-
+import TestSocioPage from '../testSocioPage/testSocioPage';
+import Test16PersPage from '../test16PersPage/test16PersPage';
 
 const App = () => {
     return (
         <Routes>
             {/* <Route exact path="/" element={<EnterPage />} /> */}
             <Route exact path="/" element={<DialogsPage />} />
+            <Route exact path="/testSocioPage" element={<TestSocioPage />} />
+            <Route exact path="/test16PersPage" element={<Test16PersPage />} />
             <Route exact path="/registrationPage" element={<RegistrationPage />} />
             <Route exact path="/editPage" element={<EditPage />} />
             {/* <Route path="/chatapp/:id"
